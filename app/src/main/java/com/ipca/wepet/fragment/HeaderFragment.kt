7a7cc7ff562adbe4e5b2c2bf25e43007ca1e77b0
João Silva <lesijoaosilva@gmail.com>
2024-05-01 21:04:09 +0100

    Review code.

--- app/src/main/java/com/ipca/wepet/fragment/HeaderFragment.kt
+++ app/src/main/java/com/ipca/wepet/fragment/HeaderFragment.kt
@@ -2,7 +2,6 @@ package com.ipca.wepet.fragment
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
